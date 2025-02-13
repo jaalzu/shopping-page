74cf56f8ec4d0671098fa158d2101f9875b5c25c
Javier <javieralzuu@gmail.com>
2025-02-13 07:09:14 -0300

    dark mode almost done

--- script.js
+++ script.js
@@ -1,4 +1,27 @@
 
+let darkmode = localStorage.getItem('darkmode');
+const themeStich = document.getElementById('theme-switch');
+
+const enableDarkMode = () => {
+    document.body.classList.add('darkmode');
+    localStorage.setItem('darkmode','active');
+
+}
+
+const disableDarkMode = () => {
+    document.body.classList.remove('darkmode');
+    localStorage.setItem('darkmode',null)
+}
+
+if(darkmode === "active") enableDarkMode();
+
+themeStich.addEventListener("click",() => {
+    darkmode = localStorage.getItem('darkmode')
+    darkmode !== "active" ? enableDarkMode() : disableDarkMode()
+})
+
+
+
 // array de productos 
 const productos = [
     {
